02319765843bb60c5e4ae1ce9de8166e027ecc2f
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-08-28 13:19:44 +0600

    Dynamic Data Load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Counter></Counter>
+		<LoadComments></LoadComments>
     </div>
   );
 }
 
+//Counter Component Create
+function Counter(){
+	//Counuter Even Handler For Increase
+	const [count, setCount] = useState(0)
+	const handleIncrease = ()=>{
+		let value = count + 1;
+		setCount(value);
+	}
+	//Counuter Even Handler For Decrease
+	const handleDecrease = ()=>{
+		let value = count - 1;
+		setCount(value);
+	}
+	return (
+		<div>
+			<h1>Counter:{count} </h1>
+			<button onClick={handleIncrease}>Increase</button>
+			<button onClick={handleDecrease}>Decrease</button>
+		</div>
+	)
+}
+
+//Create Load Comments Componenet
+function LoadComments(){
+	const [comments, setComments] = useState([])
+	useEffect(()=>{
+		fetch('https://jsonplaceholder.typicode.com/comments')
+		.then(res=>res.json())
+		.then(data=>setComments(data))
+	},[])
+	return (
+		<div>
+			<h2>Comments: {comments.length}</h2>
+			{
+				comments.map(comment=><Comment email={comment.email} body={comment.body}></Comment>)
+			}
+		</div>
+	)
+}
+function Comment(props){
+	return(
+		<div>
+			<h4>Email:{props.email}</h4>
+			<p>Comment:{props.body}</p>
+		</div>
+	)
+}
 export default App;
---
+++ src/HideCode.js
@@ -0,0 +1,14 @@
+// function Counter(){
+// 	const [count, setCounter] = useState(0)
+
+// 	const handleIncrease=()=>setCounter(count + 1);
+// 	const handleDecrease=()=>setCounter(count - 1);
+
+// 	return (
+// 		<div>
+// 			<h1>Counter:{count}</h1>
+// 			<button onClick={handleIncrease}>Increase</button>
+// 			<button onClick={handleDecrease}>Decrease</button>
+// 		</div>
+// 	)
+// }
